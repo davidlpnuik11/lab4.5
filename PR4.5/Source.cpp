2dab2f5acb173572e7d7fba5b96c6cf64486a0df
davidlpnuik11 <davidzxcneghoul@gmail>
2022-10-27 19:36:01 +0300

    lab4.5

---
+++ PR4.5/Source.cpp
@@ -0,0 +1,39 @@
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main() {
+    double x;
+    double y;
+    double R;
+    cout << "Enter R: "; cin >> R;
+    srand((unsigned)time(NULL));
+    //input
+    for (int i = 0; i <= 10; i++)
+    {
+        cout << "Enter x: "; cin >> x;
+        cout << "Enter y: "; cin >> y;
+
+        if ((R * R >= x * x + y * y))
+            cout << "yes" << endl;
+        else
+            cout << "no" << endl;
+
+    }
+    cout << "it's time for random!" << endl;
+    //random
+    for (int i = 0; i <= 10; i++)
+    {
+        x = 4 * R * rand() / RAND_MAX - 2 * R;
+        y = 4 * R * rand() / RAND_MAX - 2 * R;
+        if ((R * R >= x * x + y * y))
+
+            cout << "|x|" << setw(8) << setprecision(4) << x << "  |y|" << setw(8) << setprecision(4) << y << "  " << "yes" << endl;
+        else
+            cout << "|x|" << setw(8) << setprecision(4) << x << "  |y|" << setw(8) << y << "  " << "no" << endl;
+    }
+
+
+    return 0;
+}
